6bab4883b7f99908821b25851dbaa79ac57b6599
digimbyte <digimbyte@gmail.com>
2021-08-16 10:14:08 +1000

    fixed formatting

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 const Throw = function (err) {
-    console.warn("Log Catch", err.message || err);
-    throw new Error(err);
-  }
-  const Return = function (err) {
-      console.warn("Log Catch", err.message || err);
-      return err;
-    }
-
-const LogCatch = {
-    Throw: Throw,
-    Return: Return,
+  console.warn("Log Catch", err.message || err);
+  throw new Error(err);
+}
+const Return = function (err) {
+  console.warn("Log Catch", err.message || err);
+  return err;
 }
 
-  export default LogCatch;
+const LogCatch = {
+  Throw: Throw,
+  Return: Return,
+}
+
+export default LogCatch;
